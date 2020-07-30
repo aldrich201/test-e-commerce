d05bfc19e0caef7dbb87e1d24c153f2f0d06d1b6
aldrich201 <angaldrich31@gmail.com>
2020-07-30 19:51:50 +1000

    first commit

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,34 @@
+import React, { Component, Fragment } from 'react';
+import Title from '../Title';
+import CartColumns from './CartColumns';
+import EmptyCart from './EmptyCart';
+import { ProductConsumer } from '../../context';
+import CartList from './CartList';
+import CartTotal from './CartTotal';
+
+export default class Cart extends Component {
+	render() {
+		return (
+			<section>
+				<ProductConsumer>
+					{(value) => {
+						const { cart } = value;
+
+						if (cart.length) {
+							return (
+								<Fragment>
+									<Title name="your" title="cart" />
+									<CartColumns />
+									<CartList value={value} />
+									<CartTotal value={value} />
+								</Fragment>
+							);
+						}
+
+						return <EmptyCart />;
+					}}
+				</ProductConsumer>
+			</section>
+		);
+	}
+}
